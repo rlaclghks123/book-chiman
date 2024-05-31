239d6aa5e1bcc4b107947c27755270c726ca672f
rlaclghks123 <rlaclghks123@naver.com>
2024-05-31 20:39:43 +0900

    feat: ErrorPage 생성

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+function ErrorPage() {
+  return (
+    <Wrapper>
+      <Title>잘못된 접근입니다. 홈페이지로 이동해주세요</Title>
+    </Wrapper>
+  );
+}
+
+export default ErrorPage;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+
+  padding-top: 5rem;
+  box-sizing: border-box;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+`;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,14 @@
 import CommonLayout from './layout/CommonLayout';
+import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
 
 const routes = [
   {
     element: <CommonLayout />,
-    children: [{ path: '/', element: <HomePage /> }],
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/*', element: <ErrorPage /> },
+    ],
   },
 ];
 
