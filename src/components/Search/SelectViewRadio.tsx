abe3dec708f8739b1f1dc30ac7434e3f1e2ee281
rlaclghks123 <rlaclghks123@naver.com>
2024-06-02 14:55:03 +0900

    feat: SelectViewRadio 컴포넌트 생성

---
+++ src/components/Search/SelectViewRadio.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent } from 'react';
+import { useRecoilState } from 'recoil';
+import styled from 'styled-components';
+
+import { viewTypeState } from '../../atoms/searchAtom';
+
+function SelectViewRadio() {
+  const [curViewType, setCurViewType] = useRecoilState(viewTypeState);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCurViewType(e.target.value);
+  };
+
+  return (
+    <Wrapper>
+      <Label>
+        <input
+          type="radio"
+          name="view"
+          value="card-view"
+          onChange={handleChange}
+          checked={curViewType === 'card-view'}
+        />
+        <span>Card View</span>
+      </Label>
+
+      <Label>
+        <input
+          type="radio"
+          name="view"
+          value="list-view"
+          onChange={handleChange}
+          checked={curViewType === 'list-view'}
+        />
+        <span>List View</span>
+      </Label>
+    </Wrapper>
+  );
+}
+
+export default SelectViewRadio;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+`;
+
+const Label = styled.label`
+  cursor: pointer;
+`;
